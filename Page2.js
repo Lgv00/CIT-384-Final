3f13463ccfbd0a39dc7faacef21b7ef1dcb0bad2
Lgv00 <86891420+Lgv00@users.noreply.github.com>
2024-12-10 16:21:48 -0800

    Update Page2.js

--- Page2.js
+++ Page2.js
@@ -1,18 +1,10 @@
 <div class="cravings">
     <h2>Are you craving anything specific?</h2>
     <div>
-        <label>
-            <input type="checkbox" id="sweet"> Sweet
-        </label>
-        <label>
-            <input type="checkbox" id="savory"> Savory
-        </label>
-        <label>
-            <input type="checkbox" id="spicy"> Spicy
-        </label>
-        <label>
-            <input type="checkbox" id="salty"> Salty
-        </label>
+        <label><input type="checkbox" name="craving" value="sweet"> Sweet</label>
+        <label><input type="checkbox" name="craving" value="savory"> Savory</label>
+        <label><input type="checkbox" name="craving" value="spicy"> Spicy</label>
+        <label><input type="checkbox" name="craving" value="salty"> Salty</label>
     </div>
     <button onclick="findFood()">Find Food</button>
 </div>
